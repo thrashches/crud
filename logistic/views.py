34a359242870fc15783e132dfc0bc60ca4147468
thrashches <thrashches@yandex.ru>
2022-03-01 15:50:00 +0300

    Add pagination

--- logistic/views.py
+++ logistic/views.py
@@ -1,6 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 
+
 from django.db.models import Q
 
 from logistic.models import Product, Stock
@@ -18,7 +19,14 @@ class ProductViewSet(ModelViewSet):
         # Только для Python3.8+
         if search := request.GET.get('search'):
             queryset = queryset.filter(Q(title__icontains=search) | Q(description__icontains=search))
-        serializer = self.serializer_class(queryset, many=True)
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+
         return Response(serializer.data)
 
 
@@ -33,5 +41,10 @@ class StockViewSet(ModelViewSet):
         # Только для Python3.8+
         if products := request.GET.get('products'):
             queryset = queryset.filter(positions__product=products)
-        serializer = self.serializer_class(queryset, many=True)
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
--- stocks_products/settings.py
+++ stocks_products/settings.py
@@ -55,6 +55,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'stocks_products.urls'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -78,16 +83,16 @@ WSGI_APPLICATION = 'stocks_products.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3'
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'netology_stocks_products',
-    #     'HOST': '127.0.0.1',
-    #     'PORT': '5432',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3'
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'netology_stocks_products',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
 }
 
 
